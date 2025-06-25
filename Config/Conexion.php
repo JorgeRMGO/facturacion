d044fc4c798fcd4dfa1f02fec5c66eda7bd9a532
macc0328 <antoniocastroce03@gmail.com>
2025-06-25 11:34:51 -0600

    ajuste en conexion para conectar conexiontunel

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -1,48 +1,52 @@
 <?php 
 require_once "global.php";
+require_once "ConexionTunel.php";
 
-$conexion=new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
-mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
+$conexiontunel = new mysqli(host, usuario, contrasena, basedatos, puerto);
 
-//muestra posible error en la conexion
+
+if ($conexiontunel->connect_error) {
+    die(" Conexión por túnel fallida: " . $conexiontunel->connect_error);
+}
+
+$conexion = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+
+mysqli_query($conexion, 'SET NAMES "' . DB_ENCODE . '"');
+
 if (mysqli_connect_errno()) {
-	printf("Ups parece que falló en la conexion con la base de datos: %s\n",mysqli_connect_error());
-	exit();
+    printf("❌ Error en la conexión normal: %s\n", mysqli_connect_error());
+    exit();
 }
 
 if (!function_exists('ejecutarConsulta')) {
-	Function ejecutarConsulta($sql){ 
+
+	function ejecutarConsulta($sql){ 
 		global $conexion;
-		$query=$conexion->query($sql);
-		return $query;
+		return $conexion->query($sql);
 	} 
 
 	function ejecutarConsultaSimpleFila($sql){
 		global $conexion;
-
-		$query=$conexion->query($sql);
-		$row=$query->fetch_assoc();
-		return $row;
+		$query = $conexion->query($sql);
+		return $query->fetch_assoc();
 	}
+
 	function ejecutarConsulta_retornarID($sql){
 		global $conexion;
-		$query=$conexion->query($sql);
+		$conexion->query($sql);
 		return $conexion->insert_id;
 	}
 
 	function limpiarCadena($str){
 		global $conexion;
-		$str=mysqli_real_escape_string($conexion,trim($str));
-		return htmlspecialchars($str);
+		return htmlspecialchars(mysqli_real_escape_string($conexion, trim($str)));
 	}
-	
+
 	function limpiarJSON($str){
-    	global $conexion;
-    	return mysqli_real_escape_string($conexion, trim($str));
-    }
-	
-
+		global $conexion;
+		return mysqli_real_escape_string($conexion, trim($str));
+	}
 }
-
 ?>
