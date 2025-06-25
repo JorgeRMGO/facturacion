73db7266538acebc9fcfd810c847ef4202b5863e
macc0328 <antoniocastroce03@gmail.com>
2025-06-25 10:41:44 -0600

    Agregado de conexion,global,conexionTunel

---
+++ Config/ConexionTunel.php
@@ -0,0 +1,15 @@
+<?php
+$host = "127.0.0.1";   // Usas 127.0.0.1 si estás en el mismo server o usas túnel
+$puerto = 3306;        // O 3307 si estás usando un túnel SSH en ese puerto
+$usuario = "hikcentral";     // Cambia por tu user de la base
+$contrasena = "5GgAN7m65DRxuFgHMVun";  // Tu contraseña
+$basedatos = "employees";
+
+$conn = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+echo "🔥 Conectado con éxito a la base de datos";
+?>
